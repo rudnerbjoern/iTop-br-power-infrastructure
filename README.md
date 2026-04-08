6fae81dfb987f65a96b9af7cbf4a0c2d6ed91767
Björn Rudner <rudnerbjoern@hotmail.com>
2026-04-08 10:29:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# iTop-br-template
+
+Copyright (c) 2025 Björn Rudner
+[![License](https://img.shields.io/github/license/rudnerbjoern/iTop-br-TODO)](https://github.com/rudnerbjoern/iTop-br-TODO/blob/main/LICENSE)
+
+## What?
+
+Template for iTop Extensions
+
+## Features
+
+## Relations
+
+## Installation
+
+## Status
+
+## iTop Compatibility
+
+The extension was tested on iTop 3.2.1
+
+## Attribution
+
+This Extension uses Icons from:
---
+++ br-TODO/dictionaries/de.dict.br-TODO.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2025 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2025-06-01
+ *
+ * Localized data
+ */
+
+//
+// Class: TODO
+//
+/** @disregard P1009 Undefined type Dict */
+Dict::Add('DE DE', 'German', 'Deutsch', array(
+    'Class:TODO' => 'TODO',
+    'Class:TODO+' => '',
+    'Class:TODO/Attribute:name' => 'Name',
+    'Class:TODO/Attribute:name+' => '',
+));
